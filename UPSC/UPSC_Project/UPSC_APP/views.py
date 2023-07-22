cf87c3b43d3d4ccd67c7f8f152e084816d7153ab
raja-spec <rraja48569@gmail.com>
2023-07-22 20:23:16 +0530

    removing old code

--- UPSC/UPSC_Project/UPSC_APP/views.py
+++ UPSC/UPSC_Project/UPSC_APP/views.py
@@ -2,19 +2,12 @@ from django.shortcuts import render
 from .models import Data 
 from countryinfo import CountryInfo
 import pandas as pd
-import wikipedia
-import csv
-import pycountry
-from time import sleep
-# Create your views here.
 
-# OLD CODE
 
 def getCountry():
     data=pd.read_csv("archive/countries.csv")
     countries=[i for i in data['Country']]
     return countries
-
 
 global oldcap
 oldcap=dict()
@@ -30,35 +23,9 @@ def getCaptial():
             pass
     return oldcap
 
-def oldCountry():
-    capital=getCaptial()
-    return capital
-
-
-############################################################
-#              NEW CODE
-
-def get_all_countries():
-    all_countries = list(pycountry.countries)
-    country_names = [country.name for country in all_countries]
-    return country_names
-
-def get_country_info(country_name):
-    country_info = CountryInfo(country_name).capital()
-    return country_info
-
-all_countries = get_all_countries()
-newcaptial={}
-def Country():
-    
-    for country_name in all_countries:
-        try:newcaptial[country_name]=get_country_info(country_name)
-        except:newcaptial[country_name]="Data not found"
-    return newcaptial   
- 
 def Main(request):
-    r=[i for i in Country().values() if i=="Data not found"]
-    s=[i for i in getCaptial().values() if i=="Not found"]
-    return render(request,"home.html",{"new":r,"old":s})
+    captial=getCaptial()
+    return render(request,'home.html',{"captial":captial})
+
 
 
