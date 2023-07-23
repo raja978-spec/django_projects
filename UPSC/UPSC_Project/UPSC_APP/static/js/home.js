4000fd17ef93b3eb11dc964396410895473b69f4
raja-spec <rraja48569@gmail.com>
2023-07-23 19:27:35 +0530

    Adding styles to home

--- UPSC/UPSC_Project/UPSC_APP/static/js/home.js
+++ UPSC/UPSC_Project/UPSC_APP/static/js/home.js
@@ -1,152 +1,74 @@
-getPagination('#table-id');
-//getPagination('.table-class');
-//getPagination('table');
+document.addEventListener("DOMContentLoaded", function () {
+  const rowsPerPage = 5;
+  const table = document.getElementById("data-table");
+  const filterInput = document.getElementById("filter");
+  const prevPageBtn = document.getElementById("prevPage");
+  const nextPageBtn = document.getElementById("nextPage");
+  const currentPageSpan = document.getElementById("currentPage");
+  const totalPagesSpan = document.getElementById("totalPages");
 
-/*					PAGINATION 
-- on change max rows select options fade out all rows gt option value mx = 5
-- append pagination list as per numbers of rows / max rows option (20row/5= 4pages )
-- each pagination li on click -> fade out all tr gt max rows * li num and (5*pagenum 2 = 10 rows)
-- fade out all tr lt max rows * li num - max rows ((5*pagenum 2 = 10) - 5)
-- fade in all tr between (maxRows*PageNum) and (maxRows*pageNum)- MaxRows 
-*/
+  let currentPage = 1;
 
+  function updatePaging() {
+    const rows = table.getElementsByTagName("tr");
+    const totalRows = rows.length - 1; // Subtract 1 to exclude the header row
+    const totalPages = Math.ceil(totalRows / rowsPerPage);
 
-function getPagination(table) {
-var lastPage = 1;
+    // Ensure current page is within valid range
+    currentPage = Math.max(1, Math.min(currentPage, totalPages));
 
-$('#maxRows')
-.on('change', function(evt) {
-//$('.paginationprev').html('');						// reset pagination
+    // Hide all rows first
+    for (let i = 1; i < rows.length; i++) {
+      rows[i].style.display = "none";
+    }
 
-lastPage = 1;
-$('.pagination')
-.find('li')
-.slice(1, -1)
-.remove();
-var trnum = 0; // reset tr counter
-var maxRows = parseInt($(this).val()); // get Max Rows from select option
+    // Show only the rows for the current page
+    const startIndex = (currentPage - 1) * rowsPerPage + 1;
+    const endIndex = Math.min(startIndex + rowsPerPage, rows.length);
+    for (let i = startIndex; i < endIndex; i++) {
+      rows[i].style.display = "table-row";
+    }
 
-if (maxRows == 5000) {
-$('.pagination').hide();
-} else {
-$('.pagination').show();
-}
+    // Update pagination text
+    currentPageSpan.textContent = currentPage;
+    totalPagesSpan.textContent = totalPages;
+  }
 
-var totalRows = $(table + ' tbody tr').length; // numbers of rows
-$(table + ' tr:gt(0)').each(function() {
-// each TR in  table and not the header
-trnum++; // Start Counter
-if (trnum > maxRows) {
-// if tr number gt maxRows
+  function handleFilter() {
+    const filterValue = filterInput.value.toLowerCase();
+    const rows = table.getElementsByTagName("tr");
 
-$(this).hide(); // fade it out
-}
-if (trnum <= maxRows) {
-$(this).show();
-} // else fade in Important in case if it ..
-}); //  was fade out to fade it in
-if (totalRows > maxRows) {
-// if tr total rows gt max rows option
-var pagenum = Math.ceil(totalRows / maxRows); // ceil total(rows/maxrows) to get ..
-//	numbers of pages
-for (var i = 1; i <= pagenum; ) {
-// for each page append pagination li
-$('.pagination #prev')
-  .before(
-    '<li data-page="' +
-      i +
-      '">\
-        <span>' +
-      i++ +
-      '<span class="sr-only">(current)</span></span>\
-      </li>'
-  )
-  .show();
-} // end for i
-} // end if row count > max rows
-$('.pagination [data-page="1"]').addClass('active'); // add active class to the first li
-$('.pagination li').on('click', function(evt) {
-// on click each page
-evt.stopImmediatePropagation();
-evt.preventDefault();
-var pageNum = $(this).attr('data-page'); // get it's number
+    for (let i = 1; i < rows.length; i++) {
+      const row = rows[i];
+      const cells = row.getElementsByTagName("td");
+      let showRow = false;
 
-var maxRows = parseInt($('#maxRows').val()); // get Max Rows from select option
+      for (let j = 0; j < cells.length; j++) {
+        const cellValue = cells[j].textContent.toLowerCase();
+        if (cellValue.includes(filterValue)) {
+          showRow = true;
+          break;
+        }
+      }
 
-if (pageNum == 'prev') {
-if (lastPage == 1) {
-  return;
-}
-pageNum = --lastPage;
-}
-if (pageNum == 'next') {
-if (lastPage == $('.pagination li').length - 2) {
-  return;
-}
-pageNum = ++lastPage;
-}
+      row.style.display = showRow ? "table-row" : "none";
+    }
+    updatePaging();
+  }
 
-lastPage = pageNum;
-var trIndex = 0; // reset tr counter
-$('.pagination li').removeClass('active'); // remove active class from all li
-$('.pagination [data-page="' + lastPage + '"]').addClass('active'); // add active class to the clicked
-// $(this).addClass('active');					// add active class to the clicked
-limitPagging();
-$(table + ' tr:gt(0)').each(function() {
-// each tr in table not the header
-trIndex++; // tr index counter
-// if tr index gt maxRows*pageNum or lt maxRows*pageNum-maxRows fade if out
-if (
-  trIndex > maxRows * pageNum ||
-  trIndex <= maxRows * pageNum - maxRows
-) {
-  $(this).hide();
-} else {
-  $(this).show();
-} //else fade in
-}); // end of for each tr in table
-}); // end of on click pagination list
-limitPagging();
-})
-.val(5)
-.change();
+  function prevPage() {
+    currentPage--;
+    updatePaging();
+  }
 
-// end of on select change
+  function nextPage() {
+    currentPage++;
+    updatePaging();
+  }
 
-// END OF PAGINATION
-}
+  filterInput.addEventListener("keyup", handleFilter);
+  prevPageBtn.addEventListener("click", prevPage);
+  nextPageBtn.addEventListener("click", nextPage);
 
-function limitPagging(){
-// alert($('.pagination li').length)
-
-if($('.pagination li').length > 7 ){
-if( $('.pagination li.active').attr('data-page') <= 3 ){
-$('.pagination li:gt(5)').hide();
-$('.pagination li:lt(5)').show();
-$('.pagination [data-page="next"]').show();
-}if ($('.pagination li.active').attr('data-page') > 3){
-$('.pagination li:gt(0)').hide();
-$('.pagination [data-page="next"]').show();
-for( let i = ( parseInt($('.pagination li.active').attr('data-page'))  -2 )  ; i <= ( parseInt($('.pagination li.active').attr('data-page'))  + 2 ) ; i++ ){
-$('.pagination [data-page="'+i+'"]').show();
-
-}
-
-}
-}
-}
-
-$(function() {
-// Just to append id number for each row
-$('table tr:eq(0)').prepend('<th> ID </th>');
-
-var id = 0;
-
-$('table tr:gt(0)').each(function() {
-id++;
-$(this).prepend('<td>' + id + '</td>');
+  updatePaging();
 });
-});
-
-//  Developed By Yasser Mas
-// yasser.mas2@gmail.com
